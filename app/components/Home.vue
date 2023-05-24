7f5aa535d9847d93283ee1f567330ba26aa627c1
kurmasov01 <kurmasov01@mail.ru>
2023-05-24 19:46:16 +0300

    First commit
    Mobile development app NSnews

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,76 @@
 <template>
     <Page>
-        <ActionBar>
-            <Label text="Home"/>
-        </ActionBar>
+      <ScrollView>
+        <StackLayout class="news-container">
+          <TextField v-model="searchQuery" hint="Введите запрос"/>
+          <Label/>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
+          <Label text="Введите дату в формате ГГГГ-ММ-ДД"/>
+          <TextField v-model="selectFrom" hint="От" />
+          <TextField v-model="selectTo" hint="До" />
+          <Button text="Найти новости" @tap="fetchNews" class="btn"/>
+          <Label/>
+          <StackLayout v-for="newsItem in newsItems" :key="newsItem.title" class="news-card">
+            
+            <Image :src="newsItem.image" stretch="aspectFill" height="200" />
+            <Label :text="newsItem.title" class="title" textWrap="true" />
+            <Label :text="newsItem.description" textWrap="true" />
+            <Label :text="newsItem.publishedAt.slice(0, -10)" />
+            <Label/>
+
+          </StackLayout>
+        </StackLayout>
+      </ScrollView>
     </Page>
-</template>
-
-<script>
+  </template>
+  
+  <script>
   export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
+    data() {
+      return {
+        selectFrom: "",
+        selectTo: "",
+        searchQuery: "",
+        newsItems: [
+  
+          
+        ],
+        test: ""
+      };
+    },
+    methods: {
+      fetchNews() {
+        fetch(`https://gnews.io/api/v4/search?q=${this.searchQuery}&apikey=69efc8edf320ba963b6f92942d88039d&lang=ru&from=${this.selectFrom}T12:00:00Z&to=${this.selectTo}T12:00:00Z`)
+          .then(response => response.json())
+          .then(data => {
+            this.newsItems = data.articles;
+          })
+          .catch(error => {
+            console.error('Ошибка при получении новостей:', error);
+            console.log('Ошибка при получении новостей:', error);
+          });
+      },
     }
   };
-</script>
-
-<style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
-
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
-</style>
+  </script>
+  
+  <style scoped>
+  .news-container {
+    padding: 16px;
+  }
+  
+  .news-card {
+    background-color: #ffffff;
+    border-radius: 8px;
+    margin-bottom: 16px;
+    padding: 16px;
+  }
+  .title {
+    font-weight: bold;
+    font-size: 18px;
+    margin-bottom: 8px;
+  }
+  .btn {
+    background-color: rgb(0, 191, 216);
+  }
+  </style>
